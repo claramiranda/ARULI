8ceb0f461e9a1053eff7a8be7fba7e3916687020
Germano <maskedy@gmail.com>
2019-04-22 22:27:47 -0300

    imagens das reps, arrumando layout tb da pag delas é isto

--- app/src/main/java/br/unicamp/ft/c155041_g157918/Republicas.java
+++ app/src/main/java/br/unicamp/ft/c155041_g157918/Republicas.java
@@ -2,14 +2,14 @@ package br.unicamp.ft.c155041_g157918;
 
 public class Republicas {
     public static Republica[] republicas = {
-            new Republica("Rep Tumba","Lorem ipsum",R.drawable.tomas_dalessandro),
-            new Republica("Rep Produção","Lorem ipsum",R.drawable.tomas_dalessandro),
-            new Republica("Rep Piroca","Lorem ipsum",R.drawable.tomas_dalessandro),
-            new Republica("Rep Rota","Lorem ipsum",R.drawable.tomas_dalessandro),
-            new Republica("Rep Saia Justa","Lorem ipsum",R.drawable.tomas_dalessandro),
-            new Republica("Rep Torres Gêmeas","Lorem ipsum",R.drawable.tomas_dalessandro),
-            new Republica("Rep Peregrinos","Lorem ipsum",R.drawable.tomas_dalessandro),
-            new Republica("Rep Boa Morte","Lorem ipsum",R.drawable.tomas_dalessandro),
+            new Republica("Rep Tumba: ","Lorem ipsum",R.drawable.reptumba),
+            new Republica("Rep Produção: ","Lorem ipsum",R.drawable.repproducao),
+            new Republica("Rep Piroca: ","Lorem ipsum",R.drawable.repiroca),
+            new Republica("Rep Saia Justa: ","Lorem ipsum",R.drawable.repsaia),
+            new Republica("Rep Tcheca: ","Lorem ipsum",R.drawable.reptcheca),
+            new Republica("Rep Thug: ","Lorem ipsum",R.drawable.repthug),
+            new Republica("Rep Treze: ","Lorem ipsum",R.drawable.reptreze),
+            new Republica("Rep Peregrino: ","Lorem ipsum",R.drawable.repperegrino),
 
 
     };
